941d642d5d0dfafab8e5c34379e03e9296e450f0
Sebastián Hidalgo Vargas <hvsebas@estudiantec.cr>
2023-06-18 07:17:06 -0600

    Add Basic Admin app
    Added basic admin application

--- Servidor/src/Main.java
+++ Servidor/src/Main.java
@@ -1,7 +1,17 @@
 import java.io.IOException;
 import Server.ServerHandler;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new AdminInterface();
+            }
+        });
 
         ServerHandler serverA= new ServerHandler(9090);
 
