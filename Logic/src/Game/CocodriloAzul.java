1df0069e2f0b3e33648ba3def71976d88bdfa472
valeriam07 <valeriamorales40@gmail.com>
2023-06-19 13:16:11 -0600

    speed update

--- Logic/src/Game/CocodriloAzul.java
+++ Logic/src/Game/CocodriloAzul.java
@@ -7,6 +7,7 @@ public class CocodriloAzul extends DonCE{
     public CocodriloAzul(Integer plataforma){
         this.plataforma = plataforma;
         System.out.println("Se creo un cocodrilo azul");
+        velocidad = 100;
     }
 
     
